7c9c8d9ca4881ca140d11c237254da5e82446b82
Dayrook <derekscherer@gmail.com>
2023-06-02 10:21:44 -0500

    Bounding Volume uniforms are controllable from editor.

--- SDF_Acceleration/sdf_container_aabb.gd
+++ SDF_Acceleration/sdf_container_aabb.gd
@@ -1,6 +1,21 @@
 @tool
 class_name sdf_container_aabb extends MeshInstance3D
 
+@export var bb_center:Vector3 = Vector3(0,0,0) :
+	get:
+		return bb_center
+	set(s):
+		bb_center = s
+		_update_shader_param() 
+
+
+@export_range(0.1,2.0) var bb_size:float = .55 :
+	get:
+		return bb_size
+	set(s):
+		bb_size = s
+		_update_shader_param()
+		
 
 const SHADER_PATH = "res://SDF_Acceleration/ST_AABB.gdshader"
 
@@ -10,13 +25,13 @@ class ShaderTemplate:
 	var after_scene := ""
 
 
-var _objects : Array= []
-var _cutaways : Array= []
-var _next_id := 0
-var _shader_template : ShaderTemplate
-var _shader_material : ShaderMaterial
-var _need_shader_update := true
-var _need_objects_update := true
+#var _objects : Array= []
+#var _cutaways : Array= []
+#var _next_id := 0
+#var _shader_template : ShaderTemplate
+var _shader_material : ShaderMaterial = ShaderMaterial.new()
+#var _need_shader_update := true
+#var _need_objects_update := true
 
 
 var shrink : float = 1.0
@@ -38,18 +53,20 @@ func _ready():
 
 func _process(delta):
 	pass
-
+		
 
 
 func _update_shader():
 	var shader = load(SHADER_PATH)
-	var _shader_material = ShaderMaterial.new()
+	#var _shader_material = ShaderMaterial.new()
 	_shader_material.shader = shader
 
 	set_material_override(_shader_material)
 	
-	
 
+func _update_shader_param():
+	_shader_material.set_shader_parameter("u_bb_size", bb_size)
+	_shader_material.set_shader_parameter("u_bb_center", bb_center)
 
 func _update_aabb():
 	# TODO
